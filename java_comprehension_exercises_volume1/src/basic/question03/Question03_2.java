9a957998a1707be91c6f59ff54d8c6bb872cbd2a
nakakota <nakakota@C010423442.zipangu.cac.co.jp>
2026-04-16 14:48:13 +0900

    1-1～6-4

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
@@ -8,7 +8,17 @@ public class Question03_2 {
 
 	public static void main(String[] args) {
 
-		// TODO:ここに実装
-		
+		int basePrice = 1200;
+		int tickets = 3;
+		int reducedPrice = basePrice - 200;
+		int reduesedTickets = tickets--;
+		int totalPrice = reducedPrice * reduesedTickets;
+		double taxRate = 1.1;
+		int totalWithTax = (int) (totalPrice * taxRate);
+
+		System.out.println("購入可能枚数は" + reduesedTickets + "枚です。");
+		System.out.println("購入可能枚数は" + reducedPrice + "円です。");
+		System.out.println("合計金額(税抜)は" + totalPrice + "です。");
+		System.out.println("合計金額(税込)は" + totalWithTax + "です。");
 	}
 }
